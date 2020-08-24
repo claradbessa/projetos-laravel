1408efb391915c90494dc36b267aacbcd9b65418
Clara <43656831+claradbessa@users.noreply.github.com>
2020-08-24 07:29:45 -0300

    Routes and Controllers

---
+++ teste/resources/views/clientes/index.blade.php
@@ -0,0 +1,12 @@
+<h3>Clientes: </h3>
+<a href="{{route('clientes.create')}}">NOVO CLIENTE</a>
+
+<ol>
+    @foreach($clientes as $c)
+        <li>
+            {{$c['nome']}} |
+            <a href="{{route('clientes.edit', $c['id'])}}">Editar</a>
+            <a href="{{route('clientes.show', $c['id'])}}">Informações</a>
+        </li>
+    @endforeach
+</ol>
